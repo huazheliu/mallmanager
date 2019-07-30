4c1921d9c97d9057c5f70b10d85fa054f15228e6
huazheliu <535153924@.com>
2019-07-30 17:51:12 +0800

    完善退出功能

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -14,7 +14,7 @@
         </el-col>
         <el-col :span="2">
           <div class="grid-content bg-purple">
-            <a class="quit" href="#">退出</a>
+            <a class="quit" href="#" @click="loginout">退出</a>
           </div>
         </el-col>
       </el-row>
@@ -99,6 +99,13 @@
             const token=localStorage.getItem('token');
             if(!token){
               this.$router.push({name:'login'});
+            }
+        },
+        methods:{
+            loginout(){
+                localStorage.clear();
+                this.$message.success("退出成功");
+                this.$router.push({name:'login'});
             }
         }
     }
