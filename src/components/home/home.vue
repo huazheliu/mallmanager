46f308f7a4f39a213d04c83968dca13dd62f700e
huazheliu <535153924@.com>
2019-08-01 22:50:50 +0800

    配置权限路由

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -41,7 +41,7 @@
               <i class="el-icon-location"></i>
               <span>角色列表</span>
             </el-menu-item>
-            <el-menu-item index="1-2">
+            <el-menu-item index="rights">
               <i class="el-icon-location"></i>
               <span>权限列表</span>
             </el-menu-item>
---
+++ src/components/rights/right.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-card class="box-card">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+
+  </el-card>
+</template>
+
+<script>
+    export default {
+        name: "right"
+    }
+</script>
+
+<style scoped>
+
+</style>
